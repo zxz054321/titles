5dc2b3945477c5fa6be8d80b2b97dab355dc80d7
Abel <zxz054321@163.com>
2016-06-15 21:54:31 +0800

    Signature compatible with parent's

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,14 +2,14 @@
 
 abstract class TestCase extends Orchestra\Testbench\TestCase
 {
-    protected function getPackageProviders()
+    protected function getPackageProviders($app)
     {
         return [
             \Titles\ServiceProvider::class,
         ];
     }
 
-    protected function getPackageAliases()
+    protected function getPackageAliases($app)
     {
         return [
             'Titles' => \Titles\Facade::class,
